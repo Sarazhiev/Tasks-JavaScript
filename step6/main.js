a79a14a4ec6a1609047201b5a3fd804d6d428c9f
Sarazhiev <dilmanurs@mail.ru>
2022-02-15 00:10:31 +0600

    step6

---
+++ step6/main.js
@@ -0,0 +1,115 @@
+// Инкременты и Декременты
+// let num = 1;
+//
+// console.log(num++); // 1 постфиксный инкремент
+// console.log(num++); // 2
+// console.log(num++); // 3
+//
+// let num2 = 1;
+// console.log(++num2); // 2 префиксный инкремент
+// console.log(++num2); // 3
+// console.log(++num2); // 4
+
+// let num = 1;
+//
+// console.log(num--); // 1 постфиксный декремент
+// console.log(num--); // 0 постфиксный декремент
+// console.log(num--); // -1 постфиксный декремент
+//
+//
+// let num2 = 1;
+// console.log(--num2); // 0 префиксный декремент
+// console.log(--num2); // -1 префиксный декремент
+// console.log(--num2); // -2 префиксный декремент
+
+// let i = 0; // стартовое значение
+// while (i < 5) { // условие продолжительности цикла, пока старт меньше чем 5!
+//     i++; // шаг
+//     console.log(i) // вывод моего значение
+// }
+
+// let i = 0; // стартовое значение
+// do {
+//   i++;// шаг
+//     console.log(i) //вывод моего значения
+// } while (i < 5); // условие продолжительности цикла, пока старт меньше чем 5!
+//
+//
+//      старт    условие  шаг
+// for (let i = 0; i < 5; i--) {
+//     console.log(i)
+// }
+
+// for (let i = 0; i <= 15; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// let i = 0;
+// for (let i = 0; i <= 15; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+// let result = 0;
+// for (result += 1; result <= 20; result++) {
+//     console.log(result)
+// }
+// let result = 0;
+// for (let i = 0; i <= 10; i++) {
+//     result += i
+// }
+// console.log(result);
+// let str = 'qwerty';
+// for (let i = 0; i < str.length; i++) {
+//
+//     console.log(str[i])
+// }
+// let str = 'abracadabra';
+// for (let i = 1; i < str.length; i+=2) {
+//     console.log(str[i])
+// }
+// let str = '456788123';
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i] > 5) {
+//         console.log(str[i])
+//     }
+// }
+// let arr = [3, 3, 4, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i])
+//     }
+// }
+// let arr = [3, 3, 4, 4, 5];
+// for (let i = 0; i < arr.length; i+=2)
+//     console.log(arr[i]);
+// let arr = [1,2,3, 'Ivan', true];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] )
+//
+// }
+// let arr = ['Hello', 'Hello world', 'Hi', 'Hellooooo', 'Thanks'];
+// for (let i = 0; i < arr.length;  i++) {
+//     if (arr[i].includes('Hello')) {
+//         console.log('true')
+//     }
+// }
